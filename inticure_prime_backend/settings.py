a25447d5561b5f150a7bc4f09276c0bf775b0c9c
joel <joelbaiju98@gmail.com>
2025-07-28 19:55:00 +0530

    first commit

--- inticure_prime_backend/settings.py
+++ inticure_prime_backend/settings.py
@@ -116,12 +116,24 @@ SIMPLE_JWT = {
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'inticure',   
+#         'USER': 'root',       
+#         'PASSWORD': 'root@123',  
+#         'HOST': 'localhost',    
+#         'PORT': '3306',         
+#     }
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'inticure',   
         'USER': 'root',       
-        'PASSWORD': 'root@123',  
+        'PASSWORD': 'Berlin@123',  
         'HOST': 'localhost',    
         'PORT': '3306',         
     }
