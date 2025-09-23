6d0d42829ee4e4b81f2c8420b615f878b03d3ef6
joel <joelbaiju98@gmail.com>
2025-09-24 01:28:56 +0530

    testing emails

--- customer/views/dashboard_view.py
+++ customer/views/dashboard_view.py
@@ -92,7 +92,8 @@ def get_meet_details_with_meet_id(request):
     """
     meet_id = request.GET.get('meet_id', None)
     meet_id = uuid.UUID(meet_id)
-    logger.debug(f"......{meet_id}......")
+    logger.debug(f"......{meet_id}......2718f35d-88b1-4d68-a524-28350ca0b68f")
+    logger.debug(f"......{meet_id == "2718f35d-88b1-4d68-a524-28350ca0b68f"}")
     if not meet_id:
         return Response(
             {"error": "meet_id is required."}, 
@@ -101,7 +102,7 @@ def get_meet_details_with_meet_id(request):
     try:
         
         try:
-            tracker = Meeting_Tracker.objects.get(customer_1_meeting_id=meet_id) 
+            tracker = Meeting_Tracker.objects.get(customer_1_meeting_id="2718f35d-88b1-4d68-a524-28350ca0b68f") 
             is_customer_1 = True
         except Meeting_Tracker.DoesNotExist:
             try :
