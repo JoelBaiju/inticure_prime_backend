3d563885d8579a28494c309b68eafe26caf36c9a
joel <joelbaiju98@gmail.com>
2025-09-24 01:45:48 +0530

    testing emails

--- customer/views/dashboard_view.py
+++ customer/views/dashboard_view.py
@@ -91,6 +91,7 @@ def get_meet_details_with_meet_id(request):
     API to get meet details with meet id
     """
     meet_id = request.GET.get('meet_id', None)
+    target_id = uuid.UUID(meet_id)
     logger.debug(f"......{meet_id}......2718f35d-88b1-4d68-a524-28350ca0b68f")
     logger.debug(f"......{meet_id == '2718f35d-88b1-4d68-a524-28350ca0b68f'}")
     if not meet_id:
@@ -101,11 +102,11 @@ def get_meet_details_with_meet_id(request):
     try:
         
         try:
-            tracker = Meeting_Tracker.objects.get(customer_1_meeting_id="2718f35d-88b1-4d68-a524-28350ca0b68f") 
+            tracker = Meeting_Tracker.objects.get(customer_1_meeting_id=target_id) 
             is_customer_1 = True
         except Meeting_Tracker.DoesNotExist:
             try :
-                tracker = Meeting_Tracker.objects.get(customer_2_meeting_id = meet_id)
+                tracker = Meeting_Tracker.objects.get(customer_2_meeting_id = target_id)
                 is_customer_1 = False
             except Meeting_Tracker.DoesNotExist:
                 return Response(
