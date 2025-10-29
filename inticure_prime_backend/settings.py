2d19b7001cb8e94c45e87bb68a071e192f9e451f
joel <joelbaiju98@gmail.com>
2025-10-30 00:14:46 +0530

    the

--- inticure_prime_backend/settings.py
+++ inticure_prime_backend/settings.py
@@ -308,10 +308,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # EMAIL_HOST_PASSWORD = 'kfqk vezg lylg nbuu'  # App Password or Gmail password
 DEFAULT_FROM_EMAIL = 'wecare@inticure.com'  # Default from email (can be your Gmail or another verified email)
 
-# ADMIN_CC_EMAILS = ['wecare@inticure.com']
-ADMIN_CC_EMAILS = ['joelbaiju98@gmail.com','nikhilappu112@gmail.com']
-# ADMIN_WA_NUMBERS = ['+917483963192','+919562512875']
-ADMIN_WA_NUMBERS = ['+918891268078']
+ADMIN_CC_EMAILS = ['wecare@inticure.com']
+# ADMIN_CC_EMAILS = ['joelbaiju98@gmail.com','nikhilappu112@gmail.com']
+ADMIN_WA_NUMBERS = ['+917483963192','+919562512875']
+# ADMIN_WA_NUMBERS = ['+918891268078']
 
 
 
