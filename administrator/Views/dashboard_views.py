d938f1593d1bb225b63400c6c42b735837a923d0
joel <joelbaiju98@gmail.com>
2025-11-05 12:05:38 +0530

    the

--- administrator/Views/dashboard_views.py
+++ administrator/Views/dashboard_views.py
@@ -20,12 +20,23 @@ class Admin_dashboard(APIView):
 
 
 
-
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 class StandardPagination(PageNumberPagination):
     page_size = 20
     page_size_query_param = "page_size"
     max_page_size = 200
+
+    def get_paginated_response(self, data):
+        total_pages = self.page.paginator.num_pages if self.page else 1
+        return Response({
+            'count': self.page.paginator.count,
+            'total_pages': total_pages,
+            'next': self.get_next_link(),
+            'previous': self.get_previous_link(),
+            'results': data,
+        })
 
 
 from ..services.appointment_services import (
